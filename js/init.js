f5b170a0d7b3a65244b5d48f6729ae0ab09a4ca2
Xiaofei <zhouxf14@163.com>
2023-07-01 21:29:46 -0400

    v0

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.slider').slider();
+    $('.scrollspy').scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
